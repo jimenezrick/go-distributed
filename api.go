42a73d42a42c55d299a6e15b49e5b1070389c5a3
Ricardo Catalinas Jiménez <rcatalinas@tuenti.com>
2013-03-04 11:33:22 +0100

    Añade más detalles al diseño inicial de la API

--- api.go
+++ api.go
@@ -1,19 +1,51 @@
 import "mesh"
 
+/*
+   Me gusta el nombre de "mesh" para el paquete, porque es corto y representa un
+   poco la idea de lo que haria la libreria. Pero vamos, que es un tema totalmente
+   abierto y no es el definitivo.
+ */
 
+/*
+   Un tema que no tengo claro del todo es lo de usar un solo socket. Es lo mas sencillo
+   y elegante, pero si un canal se llena, puede bloquear todo la comunicacion entre 2
+   nodos y es una cosa que en Erlang no esta resuelta y estaria bien hacer algo al respecto.
+ */
 
 int Main() {
+	// Arrancamos la libreria, principalmente la gorutina aceptora
+	// e iniciaizamos las estructuras de datos.
+	m, err := mesh.New()
+	m.Start()
 
-	m := mesh.New()
-	m:Start()
+	/*
+	   La libreria deberia dejar parametrizar su comportamiento:
+	     - Tamaños de colas
+	     - Comportamiento cuando los canales estan llenos
+	     - Tiempo de los timeouts
+	 */
 
-
+	// Pedimos un intento de conexion asincrono.
 	m.Connect("a.com")
+	// Pedimos un intento de conexion sincrono. Esperamos hasta conseguir
+	// una conexion satisfactoria con el nodo dado. Pero eso no impide que
+	// tras conseguir la conexion, la comunicacion se rompa. Pero de todos
+	// modos a veces puede ser comodo esperar a que la conexion se establezca
+	// para no perder muchos mensajes.
+	err := m.WaitConnected("b.com")
 
-	// /stats
-	// /stats/net
-	// /stats/cpu
-	// /stats/mem
+	/*
+	   La etiqueta asociada a un mensaje es una simple ristra de bytes. Se
+	   Puede interpretar como un string. La subscripcion se basa en si el
+	   prefijo pedido encaja con la etiquita del paquete. El formato
+	   de la etiqueta es totalmente libre, pero puede ser buena ida (no obligatoria)
+	   usar un espacio de nombres similar al espacio de nombres del FS de UNIX:
+
+	     /stats
+	     /stats/net
+	     /stats/cpu
+	     /stats/mem
+	 */
 
 	c := m.Subscribe("/info")
 	c1 := m.Subscribe("/stats")
@@ -27,22 +59,22 @@ int Main() {
 		...
 	}
 
-
-
+	// El envio encola el mensaje en un canal interno que la gorutina enviadora
+	// leera de el. Se puede hacer una operacion no bloqueante de envio por si
+	// dicho canal esta lleno. O igualmente parametrizar que sucede si se intenta
+	// encolar en un canal que esta lleno.
 	m.Send(node, "/alerts", data)
-	/*
 	m.SendAll("/alerts", data)
-	*/
+	m.SendNonBlocking("/alerts", data)
 
-
-	p ! Msg
-	receive
-		{node, Res} ->
-			<<correct path>>
+	/*
+		p ! Msg
+		receive
+			{node, Res} ->
+				<<correct path>>
+				.....
+		after 1000 ->
+			<<timeout error>>
 			.....
-			.....
-	after 1000 ->
-		<<timeout error>>
-		.....
-		.....
+	 */
 }
